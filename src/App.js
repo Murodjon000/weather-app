edec76c81cbb9f5f05f9fdf59e93af021b0311c3
Murodjon <murodjon.tursunpulatov@gmail.com>
2020-12-03 14:26:12 +0500

    weather app react starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search__bar">
+        <input type="text" />
+        <span>
+          <SearchIcon />
+        </span>
+      </div>
+      <div className="main__box">
+        <h2>New York</h2>
+        <h2>23</h2>
+        <h2>Clear Sky</h2>
+        <h5>Monday 12:00pm</h5>
+      </div>
+      <div className="grid__container">
+        <div className="box1">
+          <h2>New York</h2>
+          <h2>23</h2>
+          <h2>Clear Sky</h2>
+        </div>
+        <div className="box1">
+          <h2>New York</h2>
+          <h2>23</h2>
+          <h2>Clear Sky</h2>
+        </div>
+        <div className="box1">
+          <h2>New York</h2>
+          <h2>23</h2>
+          <h2>Clear Sky</h2>
+        </div>
+        <div className="box1">
+          <h2>New York</h2>
+          <h2>23</h2>
+          <h2>Clear Sky</h2>
+        </div>
+      </div>
     </div>
   );
 }
